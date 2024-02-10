aede4f0894d388e8dd846f04e460380eaafd4bcf
aurelio-mont <aurelio.mon@me.comt>
2024-02-10 15:02:38 -0600

    updateRSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,9 @@
-import { rss } from '@astrojs/rss';
-//import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+//import { pagesGlobToRssItems } from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-  const posts = await getCollection('posts');
+  const posts = await getCollection("posts");
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
@@ -11,11 +11,10 @@ export async function GET(context) {
     //items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     items: posts.map((post) => ({
       title: post.data.title,
+      pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `posts/${post.data.slug}`,
-      pubDate: post.data.date,
-      customData: `<language>en-us</language>`,
+      link: `/posts/${post.slug}/`,
     })),
     customData: `<language>en-us</language>`,
-  });
+  })
 }

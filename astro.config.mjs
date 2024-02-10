a089c7157fa91646a08eea33ef7619d269fea875
aurelio-mont <aurelio.mon@me.comt>
2024-02-09 18:51:44 -0600

    RSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://montexpress-blog.netlify.app/"
+});

fda2be69d652de6ee8c846a390f70e3d9c665d12
aurelio-mont <aurelio.mon@me.comt>
2024-03-19 19:19:32 -0600

    new style

---
+++ src/data/navData.js
@@ -0,0 +1,25 @@
+const navData = [
+    {
+        name: "Home",
+        path: "/",
+        icon: {
+            name: "home",
+        } 
+    },
+    {
+        name: "About",
+        path: "/about/",
+        icon: {
+            name: "about",
+        } 
+    },
+    {
+        name: "Blog",
+        path: "/blog/",
+        icon: {
+            name: "blog",
+        },
+    },
+]
+
+export default navData;
